6957ccd849619016e62b24bdbb0b9271e6fb2400
asimov <gabrielcsilva0000@gmail.com>
2020-02-16 21:44:22 -0300

    criamos a classe page

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once("vendor/autoload.php");
 require_once("vendor/DB/Sql.php");
+require_once("vendor/Page.php");
 
 $app = new \Slim\Slim();
 
@@ -9,13 +10,19 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new vendor\DB\Sql();
+	// $sql = new vendor\DB\Sql();
 
-	$results = $sql->select("SELECT* FROM tb_users");
+	// $results = $sql->select("SELECT* FROM tb_users");
 
-	echo json_encode($results);
+	// echo json_encode($results);
+
+	$page = new Hcode\Page();
+
+	$page->setTpl("index");
 });
 
 $app->run();
+
+
 
  ?>
